43d5a3ca4587b32e1e05a88035dbd29c8ed9ed04
HenrikHShaya <henrik@shayasolutions.se>
2017-01-25 13:36:11 +0100

    Adding Entity Framework and DbContext file with DbSets to DataAccessLayer

---
+++ Portfolio/Portfolio.DataAccessLayer/CatalogDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Portfolio.DataAccessLayer.DomainClasses;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DataAccessLayer
+{
+    public class CatalogDbContext:DbContext
+    {
+        public CatalogDbContext() : base("name=CatalogDb")
+        {
+
+        }
+
+        public DbSet<Catalog> Catalog { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
