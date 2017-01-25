4e6426efff61e0218f00ef8dc9d23be5e59dd891
HenkeHulk <hhaggbom@gmail.com>
2017-01-25 14:41:31 +0100

    Adding IEntityRepo + Interfaces for specific domainclasses + adding Repositorys for specific domainclasses

---
+++ Portfolio/Portfolio.Repository/Interfaces/IContact.cs
@@ -0,0 +1,13 @@
+﻿using Portfolio.DataAccessLayer.DomainClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Repository.Interfaces
+{
+    public interface IContact:IEntityRepository<Contact>
+    {
+    }
+}
---
+++ Portfolio/Portfolio.Repository/Interfaces/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Repository.Interfaces
+{
+    public interface IEntityRepository<T>:IDisposable
+    {
+        IQueryable<T> All { get; }
+        T Find(int id);
+        void InsertOrUpdate(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
